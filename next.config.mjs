48f80efa44f9808e47023ec2f76829e5a312d55e
itsanubhav009 <137641994+itsanubhav009@users.noreply.github.com>
2026-08-28 22:04:21 +0000

    Add pages, contact form, S3 media storage

--- next.config.mjs
+++ next.config.mjs
@@ -2,19 +2,12 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  iimages: {
-  formats: ['image/avif', 'image/webp'],
-  remotePatterns: [
-    { protocol: 'https', hostname: 'https://pub-78cd2f72feb743b7bc8c1f93d93b0446.r2.dev' },
-  ],
-},
-  experimental: { reactCompiler: false },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      { protocol: 'https', hostname: 'https://pub-78cd2f72feb743b7bc8c1f93d93b0446.r2.dev' },
+    ],
+  },
 }
 
 export default withPayload(nextConfig)
-images: {
-  formats: ['image/avif', 'image/webp'],
-  remotePatterns: [
-    { protocol: 'https', hostname: 'pub-xxxx.r2.dev' },
-  ],
-},
