60b82aec9a94fe0cbe7d772cb34ba8d1f9b0a092
itsanubhav009 <137641994+itsanubhav009@users.noreply.github.com>
2026-08-29 01:02:21 +0000

    test

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,16 @@ const nextConfig = {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [{ protocol: 'https', hostname: 'pub-78cd2f72feb743b7bc8c1f93d93b0446.r2.dev' },],
   },
+  async headers() {
+    return [{
+      source: '/:path*',
+      headers: [
+        { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+        { key: 'X-Content-Type-Options', value: 'nosniff' },
+        { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+      ],
+    }]
+  },
 }
 
 export default withPayload(nextConfig)
